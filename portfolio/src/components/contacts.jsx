40a11a49861c5afc5692262adb224574f5ebe554
KabuyaSamuel <samuel.mwangikabuya@kibo.school>
2024-03-15 20:56:03 +0300

    update: contacts

--- portfolio/src/components/contacts.jsx
+++ portfolio/src/components/contacts.jsx
@@ -31,14 +31,14 @@ const Contacts = () => {
                           htmlFor="firstname"
                           className="tracking-wide text-white"
                         >
-                          Fistname
+                          First Name
                         </label>
                         <input
                           type="text"
                           id="fistname"
                           name="fistname"
                           autoComplete="name"
-                          placeholder="Your fistname"
+                          placeholder="Enter your first name"
                           className="mt-3 w-full border border-white/20 bg-transparent px-4 py-3 text-white/70 outline-none focus:ring-1 focus:ring-fuchsia-500"
                         />
                       </div>
@@ -47,14 +47,14 @@ const Contacts = () => {
                           htmlFor="lastname"
                           className="tracking-wide text-white"
                         >
-                          Last name
+                          Last Name
                         </label>
                         <input
                           type="text"
                           id="lastname"
                           name="lastname"
                           autoComplete="family-name"
-                          placeholder="Your last name"
+                          placeholder="Enter your last name"
                           className="mt-3 w-full border border-white/20 bg-transparent px-4 py-3 text-white/70 outline-none focus:ring-1 focus:ring-fuchsia-500"
                         />
                       </div>
@@ -64,13 +64,13 @@ const Contacts = () => {
                         htmlFor="email"
                         className="tracking-wide text-white"
                       >
-                        Mail address
+                        Email Address
                       </label>
                       <input
                         type="email"
                         id="email"
                         name="email"
-                        placeholder="Your mail address"
+                        placeholder="Enter your email address"
                         className="mt-3 w-full border border-white/20 bg-transparent px-4 py-3 text-white/70 outline-none focus:ring-1 focus:ring-fuchsia-500"
                       />
                     </div>
@@ -79,14 +79,14 @@ const Contacts = () => {
                         htmlFor="message"
                         className="tracking-wide text-white"
                       >
-                        Your message
+                        Message
                       </label>
                       <textarea
                         name="message"
                         id="message"
                         cols="30"
                         rows="6"
-                        placeholder="Your message"
+                        placeholder="Please enter your message here"
                         className="mt-3 w-full border border-white/20 bg-transparent px-4 py-3 text-white/70 outline-none focus:ring-1 focus:ring-fuchsia-500"
                         data-gramm="false"
                       ></textarea>
@@ -97,7 +97,7 @@ const Contacts = () => {
                     //   data-aos="fade-right"
                     >
                       <span className="relative uppercase tracking-wide text-white group-hover:text-fuchsia-500">
-                        Send message
+                        Send Message
                       </span>
                     </button>
                   </form>
@@ -112,7 +112,7 @@ const Contacts = () => {
                       Address
                     </h3>
                     <p className="mt-8 text-white">
-                      Riverside 25, San Francisco, California
+                      Nairobi City, Kenya
                     </p>
                   </div>
                   <div className="mt-8">
@@ -122,12 +122,12 @@ const Contacts = () => {
                     <ul className="relative z-20 mt-8 space-y-2 font-light text-white">
                       <li>
                         <a href="tel:+243000000000">
-                          Phone ------ +243 000 000 000
+                          Phone ------ +254-748-20-48-33
                         </a>
                       </li>
                       <li>
                         <a href="mailto:hello@tailus.io">
-                          E-mail ------- hello@tailus.io
+                          Email ------ samuelkabuya14@gmail.com
                         </a>
                       </li>
                     </ul>
@@ -138,12 +138,12 @@ const Contacts = () => {
                     </h3>
                     <ul className="relative z-20 mt-8 space-y-2 font-light text-white">
                       <li>
-                        <a href="#" target="_blank">
+                        <a href="https://www.linkedin.com/in/samuelkabuya/"  rel='noreferrer'target="_blank">
                           001 ------ LinkedIn
                         </a>
                       </li>
                       <li>
-                        <a href="#" target="_blank">
+                        <a href="https://github.com/KabuyaSamuel"  rel='noreferrer'target="_blank">
                           002 ------ GitHub
                         </a>
                       </li>
