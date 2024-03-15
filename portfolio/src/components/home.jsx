733e3361c658d3205bf9ba6c34236e55cdb4e93e
KabuyaSamuel <samuel.mwangikabuya@kibo.school>
2024-03-15 20:17:55 +0300

    update: Bio positioning

--- portfolio/src/components/home.jsx
+++ portfolio/src/components/home.jsx
@@ -18,10 +18,11 @@ const Home = () => {
   return (
     <div className=" min-h-screen  lg:px-56 px-10 lg:py-0 py-20 text-center gap-5 lg-text-start flex lg:flex-row flex-col-reverse justify-between lg:gap-28 items-center">
       <div data-aos="fade-up" className="h-full lg:py-40 flex flex-col justify-center lg:items-start items-center text-white">
-        <h1 className="text-[52px] font-semibold mb-8 leading-normal"> I&apos;m <span className="text-fuchsia-500"> first name last name</span></h1>
+        <h1 className="text-[52px] font-semibold mb-8 leading-normal">Hey there, I&apos;m <span className="text-fuchsia-500"> Samuel Kabuya</span></h1>
         <TypingText texts={["An AI & Automation Assistant", "Front End Developer", "Low Code & No Code Developer"]} />
-        <p data-aos="fade-left"> I&apos;m a detail-oriented accountant with a passion for technology and cybersecurity. Leveraging traditional programming languages and no-code/low-code tools, I create efficient solutions. Currently, as an Automations Assistant Trainee, I streamline business operations using no-code/low-code tools.</p> 
-        <p data-aos="fade-right"> My previous role at as a Software Engineer Intern at CodeDay Labs involved enhancing the user experience of the Zulip app. With a background in finance and accounting, I bring a unique perspective to software engineering.</p>
+        <p data-aos="fade-left">I&apos;m a detail-oriented accountant with a passion for technology and cybersecurity. Leveraging traditional programming languages and no-code/low-code tools, I create efficient solutions. Currently, as an Automations Assistant Trainee, I streamline business operations using no-code/low-code tools.</p> 
+        <p data-aos="fade-right">My previous role at as a Software Engineer Intern at <a href="https://www.codeday.org/" target='_blank' rel='noreferrer' className="text-fuchsia-500 hover:underline">CodeDay Labs</a> involved enhancing the user experience of the <a href="https://zulip.com/" target='_blank' rel='noreferrer' className="text-fuchsia-500 hover:underline">Zulip </a>open source project. With a background in finance and accounting, I bring a unique perspective to software engineering.</p>
+
         <div className="flex mt-8 gap-2">
           <div className="flex items-center justify-center">
             <div className='flex space-x-2'>
@@ -41,7 +42,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <img src={img} width={400} height={400} className='rounded-full border-2 p-1 border-fuchsia-500 img_glow' alt='' />
+      <img src={img} width={530} height={530} className='rounded-full border-2 p-1 border-fuchsia-500 img_glow' alt='' />
     </div>
   );
 };
